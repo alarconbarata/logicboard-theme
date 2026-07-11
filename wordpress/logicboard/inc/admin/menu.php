f6638b8d66af5d17cc1546a0756d5868d71859e1
alarconbarata <barata.alarcon.mkt@gmail.com>
2026-07-11 13:21:34 -0300

    feat(admin): painel LogicBoard modularizado e Hero totalmente configurável"

--- wordpress/logicboard/inc/admin/menu.php
+++ wordpress/logicboard/inc/admin/menu.php
@@ -18,3 +18,21 @@ function logicboard_admin_menu()
         30
     );
 }
+add_action('admin_enqueue_scripts', 'logicboard_admin_scripts');
+
+function logicboard_admin_scripts($hook)
+{
+    if ($hook !== 'toplevel_page_logicboard') {
+        return;
+    }
+
+    wp_enqueue_media();
+
+    wp_enqueue_script(
+        'logicboard-admin',
+        get_template_directory_uri() . '/assets/js/admin.js',
+        ['jquery'],
+        '1.0.0',
+        true
+    );
+}
